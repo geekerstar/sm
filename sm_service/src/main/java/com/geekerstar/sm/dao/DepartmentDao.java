2a013d5ac808e8e6f6c2fc719c7e744d537e598e
geekerstar <247507792@qq.com>
2018-09-19 20:08:23 +0800

    department-1

---
+++ sm_service/src/main/java/com/geekerstar/sm/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package com.geekerstar.sm.dao;
+
+import com.geekerstar.sm.entity.Department;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("departmentDao")
+public interface DepartmentDao {
+    void insert(Department department);
+    void delete(Integer id);
+    void update(Department department);
+    Department selectById(Integer id);
+    List<Department> selectAll();
+}
---
+++ sm_service/src/main/java/com/geekerstar/sm/service/DepartmentService.java
@@ -0,0 +1,14 @@
+package com.geekerstar.sm.service;
+
+import com.geekerstar.sm.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    void add(Department department);
+    void remove(Integer id);
+    void edit(Department department);
+    Department get(Integer id);
+    List<Department> getAll();
+
+}
